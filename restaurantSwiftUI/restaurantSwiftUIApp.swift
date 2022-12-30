33537737b260668c29c3cc3d53a144a806cd2e86
kyletaylor94 <kyletaylorrules94@gmail.com>
2022-12-30 10:40:01 +0100

    Initial Commit

---
+++ restaurantSwiftUI/restaurantSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurantSwiftUIApp.swift
+//  restaurantSwiftUI
+//
+//  Created by Turdesán Csaba on 2022. 12. 30..
+//
+
+import SwiftUI
+
+@main
+struct restaurantSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
